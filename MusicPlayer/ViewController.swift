decf0d592a26e912c6fc024c8d060e49cd47c79d
rahulsingh1101 <rkssingh566@gmail.com>
2024-05-15 09:44:27 +0530

    added project file

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Rahul Singh on 15/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
